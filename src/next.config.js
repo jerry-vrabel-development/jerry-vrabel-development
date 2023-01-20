d755e038ee4d11381fce6a7f97d6a03748d7781a
jerry-vrabel-development <jerry.vrabel.development@gmail.com>
2023-01-20 15:25:27 -0600

    one more time on the Github Action deployment

--- src/next.config.js
+++ src/next.config.js
@@ -4,4 +4,13 @@ const nextConfig = {
   basePath: '/jerry-vrabel-development',
 }
 
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  assetPrefix: isProd ? '/jerry-vrabel-development/' : '',
+  images: {
+    unoptimized: true,
+  },
+}
+
 module.exports = nextConfig
